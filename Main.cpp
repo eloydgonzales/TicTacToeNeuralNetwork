f437158e542aca2f026efa35f92743ade72379e9
Eloyd Lee Gonzales <egonzales164@cnm.edu>
2022-12-15 17:09:11 -0700

    moved game loop to TicTacToeGame method

--- Main.cpp
+++ Main.cpp
@@ -12,33 +12,14 @@ int main()
     std::cout << "hello \n";
 
     AIAgent player1, player2;
+    AIAgent players[10];
     RowVector input(9);
     
 
     TicTacToeGame game;
-    int playerSelection {0};
-    double output[9]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-    double largestVal{-100.0};
 
-    do
-    {
-        game.PrintBoard();
-        std::cout << game.GetCurrentPlayer() << "'s turn \n";
-
-        game.GetBoard(input);
-
-        game.NextMove(player1.GetMove(input));
-
-        game.PrintBoard();
-        std::cout << game.GetCurrentPlayer() << "'s turn \n";
-        game.GetBoard(input);
-
-        game.NextMove(player1.GetMove(input));
-    } while (!game.IsGameOver());
-
-    // display winner
-    game.PrintBoard();
-    std::cout << game.GetWinner() << " wins\n";
+    //game.PlayGame(player1, player2);
+    game.PlayGame(players[1], players[2]);
 
     return 0;
 }
--- TicTacToeGame.h
+++ TicTacToeGame.h
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <eigen3/Eigen/Eigen>
+#include "AIAgent.h"
 
 typedef Eigen::RowVectorXd RowVector;
 
@@ -16,18 +17,20 @@ public:
     TicTacToeGame(/* args */);
     ~TicTacToeGame();
 
-    bool NextMove(int playerSelection);
+    void NextMove(int playerSelection);
     void PrintBoard();
 
     Space GetCurrentPlayer(){return currentPlayer;};
     void GetBoard(RowVector& board);
     bool IsGameOver(){return gameOver;};
     Space GetWinner(){return winner;};
+    void PlayGame(AIAgent &player1, AIAgent &player2);
 private:
     Space board[9]{none, none, none, none, none, none, none, none, none};
     Space currentPlayer {xplayer};
     Space winner {none};
     bool gameOver {false};
+    int moves {0};
 
     bool CheckForWin();
 };
@@ -40,22 +43,33 @@ TicTacToeGame::~TicTacToeGame()
 {
 }
 
-bool TicTacToeGame::NextMove(int playerSelection)
+void TicTacToeGame::NextMove(int playerSelection)
 {
     if (gameOver)
     {
-        return false;
+        return;
     }
     
     if (board[playerSelection] == none)
     {
         board[playerSelection] = currentPlayer;
+        moves++;
+
         gameOver = CheckForWin();
         if (gameOver)
         {
             winner = currentPlayer;
-            return false;
+            return;
         }
+
+        // check for tie
+        if (moves >= 9)
+        {
+            gameOver = true;
+            return;
+        }
+
+        // switch current player
         if (currentPlayer == xplayer)
         {
             currentPlayer = oplayer;
@@ -64,9 +78,7 @@ bool TicTacToeGame::NextMove(int playerSelection)
         {
             currentPlayer = xplayer;
         }
-        return true;
     }
-    return false;
 }
 
 bool TicTacToeGame::CheckForWin()
@@ -123,6 +135,42 @@ void TicTacToeGame::GetBoard(RowVector& board)
         }
     }
     
+}
+
+void TicTacToeGame::PlayGame(AIAgent &player1, AIAgent &player2)
+{
+    RowVector input(9);
+    do
+    {
+        PrintBoard();
+        std::cout << currentPlayer << "'s turn \n";
+
+        GetBoard(input);
+
+        if (currentPlayer == 1)
+        {
+            NextMove(player1.GetMove(input));
+        }
+        else
+        {
+            NextMove(player2.GetMove(input));
+        }
+    } while (!IsGameOver());
+
+    // update wins
+    if (winner == 1)
+    {
+        player1.IncrementWins();
+    }
+    else
+    {
+        player2.IncrementWins();
+    }
+    
+
+    // display winner
+    PrintBoard();
+    std::cout << winner << " wins\n";
 }
 
 #endif
